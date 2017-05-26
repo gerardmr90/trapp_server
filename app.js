393828a457b236e93523526215b7d541e9e0fc2a
Gerard Montilla Romia <gerardmr90@gmail.com>
2017-05-26 11:11:28 +0200

    trying

--- app.js
+++ app.js
@@ -27,25 +27,25 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-// error handler
-app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
-});
+// app.use('/', index);
+//
+// // catch 404 and forward to error handler
+// app.use(function (req, res, next) {
+//     var err = new Error('Not Found');
+//     err.status = 404;
+//     next(err);
+// });
+//
+// // error handler
+// app.use(function (err, req, res, next) {
+//     // set locals, only providing error in development
+//     res.locals.message = err.message;
+//     res.locals.error = req.app.get('env') === 'development' ? err : {};
+//
+//     // render the error page
+//     res.status(err.status || 500);
+//     res.render('error');
+// });
 
 module.exports = app;
 //
@@ -58,10 +58,6 @@ var deliveryNextId = 1;
 var courierNextId = 1;
 
 app.use(bodyParser.json());
-
-app.get('/', function (req, res) {
-    res.send('Trapp API Root');
-});
 
 // GET /todos?completed=false&q=work
 app.get('/todos', function (req, res) {
