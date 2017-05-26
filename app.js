728635f87cac34f0f044d8b6275fb28545c1104b
Gerard Montilla Romia <gerardmr90@gmail.com>
2017-05-26 12:22:32 +0200

    GET /deliveries

--- app.js
+++ app.js
@@ -83,29 +83,58 @@ app.get('/todos', function (req, res) {
     });
 });
 
-// // GET /todos?completed=false&q=work
-// app.get('/deliveries', function (req, res) {
-//     var query = req.query;
-//     var where = {};
-//
-//     if (query.hasOwnProperty('completed') && query.completed === 'true') {
-//         where.completed = true;
-//     } else if (query.hasOwnProperty('completed') && query.completed === 'false') {
-//         where.completed = false;
-//     }
-//
-//     if (query.hasOwnProperty('q') && query.q.length > 0) {
-//         where.description = {
-//             $like: '%' + query.q + '%'
-//         };
-//     }
-//
-//     db.delivery.findAll({where: where}).then(function (deliveries) {
-//         res.json(todos);
-//     }, function (e) {
-//         res.status(500).send();
-//     });
-// });
+// GET /todos?completed=false&q=work
+app.get('/deliveries', function (req, res) {
+    var query = req.query;
+    var where = {};
+
+    if (query.hasOwnProperty('q') && query.q.length > 0) {
+        where.delivery_uid = {
+            $like: '%' + query.q + '%'
+        };
+    }
+    if (query.hasOwnProperty('q') && query.q.length > 0) {
+        where.courier_uid = {
+            $like: '%' + query.q + '%'
+        };
+    }
+    if (query.hasOwnProperty('q') && query.q.length > 0) {
+        where.receiver_uid = {
+            $like: '%' + query.q + '%'
+        };
+    }
+    if (query.hasOwnProperty('q') && query.q.length > 0) {
+        where.company_uid = {
+            $like: '%' + query.q + '%'
+        };
+    }
+    if (query.hasOwnProperty('q') && query.q.length > 0) {
+        where.company_name = {
+            $like: '%' + query.q + '%'
+        };
+    }
+    if (query.hasOwnProperty('q') && query.q.length > 0) {
+        where.address = {
+            $like: '%' + query.q + '%'
+        };
+    }
+    if (query.hasOwnProperty('q') && query.q.length > 0) {
+        where.date = {
+            $like: '%' + query.q + '%'
+        };
+    }
+    if (query.hasOwnProperty('q') && query.q.length > 0) {
+        where.state = {
+            $like: '%' + query.q + '%'
+        };
+    }
+
+    db.delivery.findAll({where: where}).then(function (deliveries) {
+        res.json(todos);
+    }, function (e) {
+        res.status(500).send();
+    });
+});
 //
 // // GET /todos?completed=false&q=work
 // app.get('/couriers', function (req, res) {
