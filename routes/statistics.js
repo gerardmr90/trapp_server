526dbb08004c0457fa08732a99b958df0e344027
victor <victor.pardos1@gmail.com>
2017-05-28 12:41:59 +0200

    created table statistics

--- routes/statistics.js
+++ routes/statistics.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../db.js');
 
-/* GET home page. */
+var app = router;
+
+
+
 router.get('/', function(req, res, next) {
-    res.render('statistics', { title: 'Statistics' });
+
+
+    db.delivery.findAll({where: {state:'Delivered'} }).done(function (deliveries) {
+        var count = 0;
+        var statisticsbyuser = new Object();
+        var keys = new Array();
+
+        for(var j = 0 ; j < deliveries.length; j++){
+            keys.push(deliveries[j].courier_uid);
+            statisticsbyuser[deliveries[j].courier_uid] = 0;
+        }
+
+        for(var i = 0; i < deliveries.length; i++){
+            count++;
+            statisticsbyuser[deliveries[i].courier_uid]++;
+        }
+
+        res.render('statistics', { keys: keys, dictionary : statisticsbyuser });
+    });
+
+
 });
+
 
 module.exports = router;
